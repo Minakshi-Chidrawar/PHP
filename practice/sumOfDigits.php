98697c0abfab430db3e73a42e03b91070533f2f1
Minakshi-Chidrawar <minakshichidrawar@gmail.com>
2019-02-27 20:20:43 +0000

    With foreach loop

--- practice/sumOfDigits.php
+++ practice/sumOfDigits.php
@@ -1,10 +1,12 @@
 <?php
 	function sum_of_digits($nums) {
+		$nums_array = str_split($nums);
 		$digits_sum = 0;
-		  for ($i = 0; $i < strlen($nums); $i++) {
-				 $digits_sum += $nums[$i];
-				   }
-		  return $digits_sum;
+		foreach($nums_array as $index => $value){
+			$digits_sum += $value;
+		}
+		
+		return $digits_sum;
 	}
 	echo "Sum of digits of 12345 is: " . sum_of_digits("12345") . "<br>";
 	echo "Sum of digits of 9999 is: " . sum_of_digits("9999") . "<br>";
