ca5e9796cff092844d2e1c3ed2750f0def5cc7d9
Minakshi-Chidrawar <minakshichidrawar@gmail.com>
2019-02-28 20:24:46 +0000

    Compute the amount of the debt in n months

---
+++ practice/fscanf.php
@@ -0,0 +1,10 @@
+<?php
+	$file = fopen("input.txt","r");
+	fscanf($file, '%d', $n);
+	$debt = 100000;
+	 
+	for($i=0; $i<$n; $i++){
+	  $debt = ceil(($debt * 1.05)/1000) * 1000;
+	}
+	echo "Amount of debt: ";
+	echo $debt. PHP_EOL;
