947e7ce854fc54d27091241df42f2b2b353a227e
Minakshi-Chidrawar <minakshichidrawar@gmail.com>
2019-03-02 21:13:25 +0000

    Sum of the first 25 prime numbers

---
+++ practice/sumFirst25Prime.php
@@ -0,0 +1,29 @@
+<?php
+	$max = 105000;
+	$file = fopen("input.txt","r");
+	$arr = new \SplFixedArray($max + 1);
+	for ($i = 2; $i <= $max; $i++) {
+		$arr[$i] = 1;
+	}
+	for ($i = 2, $len = sqrt($max); $i <= $len; $i++) {
+		if (!$arr[$i]) {
+			continue;
+		}
+		for ($j = $i, $len2 = $max / $i; $j <= $len2; $j++) {
+			$arr[$i * $j] = 0;
+		}
+	} 
+	$line = trim(fgets($file));
+	$n = (int)$line;
+	$result = 0;
+	$cnt = 0;
+	for ($i = 2; $i <= $max; $i++) {
+		if ($cnt === $n) {
+			break;
+		} elseif ($arr[$i]) {
+			$result += $i;
+			$cnt++;
+		}
+	}
+	echo "Sum of first " . $n . " prime numbers:";
+	echo $result, PHP_EOL;
