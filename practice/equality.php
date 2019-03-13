9f01b13f7190e7b5bdab6aa0b716de3253fbd792
Minakshi-Chidrawar <minakshichidrawar@gmail.com>
2019-03-13 20:30:37 +0000

    Convert the value of a PHP variable to string

---
+++ practice/equality.php
@@ -0,0 +1,12 @@
+<?php
+	$x =  20;            // $x is an integer
+	$str1 = (string)$x; // $str1 is a string now
+	// Check whether $x and $str1 are equal or not
+	if ($x === $str1) 
+	{
+	  echo "They are the same";
+	}
+	else
+	{
+	echo "They are not same";
+	}
